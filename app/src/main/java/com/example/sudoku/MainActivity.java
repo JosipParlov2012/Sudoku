352569ea4c78cee70720b1ef9f278bf9e9895778
JosipParlov2012 <josipparlov2012@gmail.com>
2021-04-11 01:01:04 +0200

    Use double matrix instead of 2d array

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -11,17 +11,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-	private static final int[] ID_CELLS = new int[] {
-			R.id.Row1Column1, R.id.Row1Column2, R.id.Row1Column3, R.id.Row1Column4, R.id.Row1Column5, R.id.Row1Column6, R.id.Row1Column7, R.id.Row1Column8, R.id.Row1Column9,
-			R.id.Row2Column1, R.id.Row2Column2, R.id.Row2Column3, R.id.Row2Column4, R.id.Row2Column5, R.id.Row2Column6, R.id.Row2Column7, R.id.Row2Column8, R.id.Row2Column9,
-			R.id.Row3Column1, R.id.Row3Column2, R.id.Row3Column3, R.id.Row3Column4, R.id.Row3Column5, R.id.Row3Column6, R.id.Row3Column7, R.id.Row3Column8, R.id.Row3Column9,
-			R.id.Row4Column1, R.id.Row4Column2, R.id.Row4Column3, R.id.Row4Column4, R.id.Row4Column5, R.id.Row4Column6, R.id.Row4Column7, R.id.Row4Column8, R.id.Row4Column9,
-			R.id.Row5Column1, R.id.Row5Column2, R.id.Row5Column3, R.id.Row5Column4, R.id.Row5Column5, R.id.Row5Column6, R.id.Row5Column7, R.id.Row5Column8, R.id.Row5Column9,
-			R.id.Row6Column1, R.id.Row6Column2, R.id.Row6Column3, R.id.Row6Column4, R.id.Row6Column5, R.id.Row6Column6, R.id.Row6Column7, R.id.Row6Column8, R.id.Row6Column9,
-			R.id.Row7Column1, R.id.Row7Column2, R.id.Row7Column3, R.id.Row7Column4, R.id.Row7Column5, R.id.Row7Column6, R.id.Row7Column7, R.id.Row7Column8, R.id.Row7Column9,
-			R.id.Row8Column1, R.id.Row8Column2, R.id.Row8Column3, R.id.Row8Column4, R.id.Row8Column5, R.id.Row8Column6, R.id.Row8Column7, R.id.Row8Column8, R.id.Row8Column9,
-			R.id.Row9Column1, R.id.Row9Column2, R.id.Row9Column3, R.id.Row9Column4, R.id.Row9Column5, R.id.Row9Column6, R.id.Row9Column7, R.id.Row9Column8, R.id.Row9Column9
+	private static final int[][] ID_CELLS = new int[][] {
+			{R.id.Row1Column1, R.id.Row1Column2, R.id.Row1Column3, R.id.Row1Column4, R.id.Row1Column5, R.id.Row1Column6, R.id.Row1Column7, R.id.Row1Column8, R.id.Row1Column9},
+			{R.id.Row2Column1, R.id.Row2Column2, R.id.Row2Column3, R.id.Row2Column4, R.id.Row2Column5, R.id.Row2Column6, R.id.Row2Column7, R.id.Row2Column8, R.id.Row2Column9},
+			{R.id.Row3Column1, R.id.Row3Column2, R.id.Row3Column3, R.id.Row3Column4, R.id.Row3Column5, R.id.Row3Column6, R.id.Row3Column7, R.id.Row3Column8, R.id.Row3Column9},
+			{R.id.Row4Column1, R.id.Row4Column2, R.id.Row4Column3, R.id.Row4Column4, R.id.Row4Column5, R.id.Row4Column6, R.id.Row4Column7, R.id.Row4Column8, R.id.Row4Column9},
+			{R.id.Row5Column1, R.id.Row5Column2, R.id.Row5Column3, R.id.Row5Column4, R.id.Row5Column5, R.id.Row5Column6, R.id.Row5Column7, R.id.Row5Column8, R.id.Row5Column9},
+			{R.id.Row6Column1, R.id.Row6Column2, R.id.Row6Column3, R.id.Row6Column4, R.id.Row6Column5, R.id.Row6Column6, R.id.Row6Column7, R.id.Row6Column8, R.id.Row6Column9},
+			{R.id.Row7Column1, R.id.Row7Column2, R.id.Row7Column3, R.id.Row7Column4, R.id.Row7Column5, R.id.Row7Column6, R.id.Row7Column7, R.id.Row7Column8, R.id.Row7Column9},
+			{R.id.Row8Column1, R.id.Row8Column2, R.id.Row8Column3, R.id.Row8Column4, R.id.Row8Column5, R.id.Row8Column6, R.id.Row8Column7, R.id.Row8Column8, R.id.Row8Column9},
+			{R.id.Row9Column1, R.id.Row9Column2, R.id.Row9Column3, R.id.Row9Column4, R.id.Row9Column5, R.id.Row9Column6, R.id.Row9Column7, R.id.Row9Column8, R.id.Row9Column9}
 	};
+
 	private static final int[] ID_NUMBERS = new int[] {
 			R.id.Number0,
 			R.id.Number1,
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 			R.id.Number9
 	};
 
-	private List<TextView> textCells;
+	private List<List<TextView>> textCells;
 	private List<Button> buttonsNumbers;
 	private Button buttonDelete;
 	private Button buttonRestart;
@@ -50,8 +51,12 @@ public class MainActivity extends AppCompatActivity {
 
 	private void initiateWidgets() {
 		textCells = new ArrayList<>();
-		for (int ID : ID_CELLS) {
-			textCells.add(findViewById(ID));
+		for (int[] IDRows : ID_CELLS) {
+			List<TextView> rowList = new ArrayList<>();
+			for (int ID : IDRows) {
+				rowList.add(findViewById(ID));
+			}
+			textCells.add(rowList);
 		}
 
 		buttonsNumbers = new ArrayList<>();
