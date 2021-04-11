c8a015e2cb27c4dbdcbaa4afbca585c3d0b9a25d
JosipParlov2012 <josipparlov2012@gmail.com>
2021-04-11 20:55:13 +0200

    Implemented number input and game set-up

---
+++ app/src/main/java/com/example/sudoku/GameHandler.java
@@ -0,0 +1,72 @@
+package com.example.sudoku;
+
+import java.util.List;
+
+import de.sfuhrm.sudoku.Riddle;
+import de.sfuhrm.sudoku.Creator;
+
+import android.graphics.Color;
+import android.widget.TextView;
+import android.annotation.SuppressLint;
+
+public class GameHandler {
+
+    private static final int COLOR_LOCKED = Color.rgb(141, 50, 168);
+
+    private static List<List<TextView>> textCells;
+
+    private static Riddle riddle;
+
+    private GameHandler() {}
+
+    public static void initiate(List<List<TextView>> textCells) {
+        GameHandler.textCells = textCells;
+
+        for (List<TextView> rowData : textCells) {
+            for (TextView textView : rowData) {
+                textView.setOnClickListener(view -> {
+                    TextView cell = (TextView) view;
+                    String value = InputButtonHandler.getActiveValue();
+
+                    cell.setText(value);
+                    cell.setTextColor(value.equals("0") ? Color.WHITE : Color.BLACK);
+
+                    saveToData();
+                });
+            }
+        }
+
+        createNewGame();
+    }
+
+    @SuppressLint("SetTextI18n")
+    public static void createNewGame() {
+        riddle = Creator.createRiddle(Creator.createFull());
+
+        // Apply data to display.
+        byte[][] array = riddle.getArray();
+        for (int row = 0; row < array.length; row++) {
+            byte[] rowData = array[row];
+            for (int column = 0; column < rowData.length; column++) {
+                byte value = rowData[column];
+                TextView cell = textCells.get(row).get(column);
+                boolean isWritable = value == 0;
+                cell.setText(value + "");
+                cell.setClickable(isWritable);
+                cell.setTextColor(isWritable ? Color.WHITE : COLOR_LOCKED);
+            }
+        }
+    }
+
+    private static void saveToData() {
+        for (int row = 0; row < textCells.size(); row++) {
+            List<TextView> rowData = textCells.get(row);
+            for (int column = 0; column < rowData.size(); column++) {
+                String text = rowData.get(column).getText().toString();
+                byte value = text.isEmpty() ? 0 : Byte.parseByte(text);
+                riddle.set(row, column, value);
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -47,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		initiateWidgets();
 		InputButtonHandler.initiate(buttonsNumbers, buttonDelete);
+		GameHandler.initiate(textCells);
+		buttonRestart.setOnClickListener(v -> GameHandler.createNewGame());
 	}
 
 	private void initiateWidgets() {
